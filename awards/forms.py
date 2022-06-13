ace4c8d8674f535f4c7f8833f13e712a933229f9
Dindihub <sadindi03@gmail.com>
2022-06-13 23:30:47 +0300

    Code-Create form for rating

--- awards/forms.py
+++ awards/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, Project, Comment
+from .models import Profile, Project, Comment,Ratings
+from django.forms import ModelForm, TextInput, EmailInput,ImageField, Textarea
 
 
 
@@ -15,9 +16,34 @@ class RegisterUserForm(UserCreationForm):
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image','bio','contact','email']
+        fields = ('image','bio','contact','email')
+        # widgets = {
+        #     'image':forms.ImageField(attrs={
+        #         'class': "form-control",
+        #         }),
+        #     'bio':forms.Textarea(attrs={
+        #         'class': "form-control", 
+        #         'placeholder': 'bio',
+        #         }),
+        #     'email':forms.EmailInput(attrs={
+        #         'class': "form-control", 
+        #         'style': 'max-width: 300px;',
+        #         'placeholder': 'Email',
+        #         })
+        # }
+
+
 
 class UploadProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ('project_image','project_title','description','like','project_url')
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Ratings
+        design_rating = forms.IntegerField()
+        usability_rating = forms.IntegerField()
+        content_rating = forms.IntegerField()
+        comment = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control","placeholder": "Leave a comment"}))    
+        exclude =['project','author']    
